4ca675d011ab2b25d21e3786f035d43d85a31303
Mahtte94 <mahtias.jebrand@live.se>
2024-12-06 19:50:18 +0100

    commit

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const fetchData = async () => {
       "https://ddragon.leagueoflegends.com/cdn/14.3.1/data/en_US/champion.json"
     );
     championsData = await res.json();
-
-    displayAllClasses();
   } catch (error) {
     console.error("Error fetching champion data:", error);
   }
@@ -43,51 +41,11 @@ const displayRandomChampion = () => {
 
   const figure = createChampionElement(randomChampion);
   container.appendChild(figure);
+
+  
 };
 
-const displayAllClasses = () => {
-  const section = document.getElementById("class-select");
-
-  const uniqueTags = new Set();
-
-  Object.keys(championsData.data).forEach((key) => {
-    const champion = championsData.data[key];
-
-    champion.tags.forEach((tag) => {
-      uniqueTags.add(tag);
-    });
-  });
-
-  uniqueTags.forEach((tag) => {
-    const option = document.createElement("option");
-    option.value = tag;
-    option.textContent = tag;
-    section.appendChild(option);
-  });
-
-  const displayRandomChampionByClass = () => {
-    const container = document.getElementById("champion-container");
-    const selectedClass = document.getElementById("class-select").value;
-    container.innerHTML = "";
-
-    const matchingChampions = Object.keys(championsData.data).filter((key) => {
-      const champion = championsData.data[key];
-      return champion.tags.includes(selectedClass);
-    });
-
-    if (matchingChampions.length > 0) {
-      const randomKey =
-        matchingChampions[Math.floor(Math.random() * matchingChampions.length)];
-      const randomChampion = championsData.data[randomKey];
-      const figure = createChampionElement(randomChampion);
-      container.appendChild(figure);
-    } else {
-      displayRandomChampion();
-    }
-  };
-
-  const randomButton = document.getElementById("random-champion-btn");
-  randomButton.addEventListener("click", displayRandomChampionByClass);
-};
+const randomButton = document.getElementById('random-champion-btn');
+randomButton.addEventListener("click", displayRandomChampion);
 
 fetchData();
