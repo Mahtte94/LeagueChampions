4bac41fb5de18f76f8bca2d350a7a2f51481b2f5
Mahtte94 <mahtias.jebrand@live.se>
2024-12-08 17:36:00 +0100

    trying to filter out specific classes with checkboxes, added box-shadow on img, moved classes to under name, added favicon

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const fetchData = async () => {
       "https://ddragon.leagueoflegends.com/cdn/14.3.1/data/en_US/champion.json"
     );
     championsData = await res.json();
-    displayClasses();
+
+    displayAllClasses();
   } catch (error) {
     console.error("Error fetching champion data:", error);
   }
@@ -21,7 +22,7 @@ const createChampionElement = (champion) => {
 
   img.src = imgUrl;
   img.alt = championName;
-  figcaption.textContent = championName + " " + "(" + classes + ")";
+  figcaption.innerHTML = `${championName}<br>(${classes})`;
 
   figure.appendChild(img);
   figure.appendChild(figcaption);
@@ -42,34 +43,50 @@ const displayRandomChampion = () => {
   container.appendChild(champion);
 };
 
+const displayRandomChampionByClass = () => {
+  const container = document.getElementById("champion-container");
+  container.innerHTML = "";
 
+  const checkedInputs = document.querySelectorAll('input[type="checkbox"]:checked');
 
-const createClassesElement = (tags) => {
-  const classContainer = document.querySelector("div");
-  classContainer.id = "class-container";
-  
-  tags.forEach(tag => {
-    const classElement = document.createElement("div");
-    classElement.textContent = tag;
-    classElement.classList.add("class-tag");
-    classContainer.appendChild(classElement);
-});
+  const championKeys = Object.keys(championsData.data);
+  const randomKey =
+    championKeys[Math.floor(Math.random() * championKeys.length)];
+  const randomChampion = championsData.data[randomKey];
 
-
+  const champion = createChampionElement(randomChampion);
+  container.appendChild(champion);
 };
 
-const displayClasses = () => {
+const displayAllClasses = () => {
+  const classContainer = document.querySelector(".class-container");
 
-  let uniqueClasses = new Set();
-  Object.values(championsData.data).forEach((champion) => {
-    champion.tags.forEach((tag) => uniqueClasses.add(tag))
+  const champs = Object.values(championsData.data);
+
+  const uniqueTags = new Set();
+  champs.forEach((champ) => {
+    champ.tags.forEach((tag) => {
+      uniqueTags.add(tag);
+    });
   });
-  
-  createClassesElement(Array.from(uniqueClasses));
- 
+
+  const uniqueTagsArrays = Array.from(uniqueTags);
+
+  uniqueTagsArrays.forEach((array) => {
+    const classes = document.createElement("input");
+    const label = document.createElement("label");
+    label.setAttribute("for", array);
+    label.textContent = array;
+    classes.type = "checkbox";
+    classes.name = array;
+    classes.id = array;
+console.log(array)
+    classContainer.appendChild(classes);
+    classContainer.appendChild(label);
+  });
 };
 
 const randomButton = document.getElementById("random-champion-btn");
-randomButton.addEventListener("click", displayRandomChampion);
+randomButton.addEventListener("click", displayRandomChampionByClass);
 
 fetchData();
